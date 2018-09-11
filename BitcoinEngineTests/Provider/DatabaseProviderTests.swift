225a1ef82c81d37e5d19f9bc4e13bc03a757184b
Ivan Ruiz Monjo <ivanruizmonjo@gmail.com>
2018-09-11 17:24:45 +0200

    added provider test

---
+++ BitcoinEngineTests/Provider/DatabaseProviderTests.swift
@@ -0,0 +1,35 @@
+//
+//  DatabaseProviderTests.swift
+//  BitcoinEngineTests
+//
+//  Created by Ivan Ruiz Monjo on 11/09/2018.
+//  Copyright © 2018 Netquest. All rights reserved.
+//
+
+import XCTest
+import Nimble
+@testable import BitcoinEngine
+
+final class DatabaseProviderTests: XCTestCase {
+    
+    private var sut: DatabaseProvider!
+    
+    override func setUp() {
+        super.setUp()
+        sut = DatabaseProvider()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSavesAndRetreivesSameBitcoinPrices() {
+        let bitcoinPrices = [BitcoinPrice(date: "1", price: 100.5), BitcoinPrice(date: "2", price: 200)]
+        
+        sut.saveBitcoinPrices(prices: bitcoinPrices)
+        
+        expect(self.sut.bitCoinPrices()).to(equal(bitcoinPrices))
+    }
+    
+}
